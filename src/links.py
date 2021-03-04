20e8fb8f49e4c84786d51060e3a3547e109f679d
moesmoesie <mndarwesh@protonmail.com>
2021-03-04 15:27:38 +0100

    Modify create_income_statements_link to use all fields if no field value is provided

--- src/links.py
+++ src/links.py
@@ -1,10 +1,13 @@
+from os import name
 from time import time
 
-
-
-def create_income_statements_link(company_symbol,fields=["annualTotalRevenue"]):
+def create_income_statements_link(company_symbol,fields=None):
     ''' Returns link for extracting data of company income statements on Yahoo Finance'''
 
+    if fields == None:
+        with open("data/income_statement_fields.txt") as parameter_file:
+            fields = [value.strip() for value in parameter_file.readlines()]
+        
     base_url = f"https://query1.finance.yahoo.com/ws/fundamentals-timeseries/v1/finance/timeseries/{company_symbol}?"
     currentTimeStamp = str(time()).split(".")[0]    
 
@@ -14,3 +17,6 @@ def create_income_statements_link(company_symbol,fields=["annualTotalRevenue"]):
 
     full_url = base_url + fields_parameters + date_parameters + domain_parameters
     return full_url
+
+if __name__ == '__main__':
+    link = create_income_statements_link("GOOG")
