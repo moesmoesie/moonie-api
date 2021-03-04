960551c2468e15d7d046ae63db2941a37df7ade8
moesmoesie <mndarwesh@protonmail.com>
2021-03-04 17:05:14 +0100

    Add function to create company stock data url

--- src/links.py
+++ src/links.py
@@ -20,3 +20,16 @@ def create_income_statements_link(company_symbol,fields=None):
 
 if __name__ == '__main__':
     link = create_income_statements_link("GOOG")
+
+
+def create_stock_price_link(company_symbol):
+    ''' Returns link for extracting data of company income statements on Yahoo Finance'''
+
+    currentTimeStamp = str(time()).split(".")[0]    
+    full_url = f"https://query1.finance.yahoo.com/v7/finance/download/{company_symbol}?period1=860371200&period2={currentTimeStamp}&interval=1d&events=history&includeAdjustedClose=true"
+
+    return full_url
+
+if __name__ == '__main__':
+    link = create_stock_price_link("GOOG")
+    print(link)
